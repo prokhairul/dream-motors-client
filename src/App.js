db96e1ed8e41c268b31a0bf365ca240f7eec67db
vakhairulislam <tubebusy@gmail.com>
2022-05-24 06:59:00 +0600

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Pages/Shared/Header';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
 
 function App() {
   return (
     <div>
-      <h2>Welcom to Dream Motors</h2>
+      <Header></Header>
+
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/Shared/Header.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import logo from '../../assets/images/logo.png'
+
+const Header = () => {
+
+    const liItems = <>
+        <li className='hover:bg-gray-600 rounded-bl rounded-tr'><a>Home</a></li>
+        <li className='hover:bg-gray-600 rounded-bl rounded-tr'><a>About Us</a></li>
+        <li className='hover:bg-gray-600 rounded-bl rounded-tr'><a>Blogs</a></li>
+        <li className='hover:bg-gray-600 rounded-bl rounded-tr'><a>Register</a></li>
+        <li className='hover:bg-gray-600 rounded-bl rounded-tr'><a>login</a></li>
+    </>
+
+    return (
+
+        <div class="navbar bg-gray-900">
+            <div class="navbar-start pl-6">
+                <div class="dropdown">
+                    <label tabindex="0" class="btn btn-ghost lg:hidden text-white">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 16 16" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        {liItems}
+                    </ul>
+                </div>
+                <a class="btn btn-ghost normal-case text-xl"> <img src={logo} /></a>
+            </div>
+
+            <div class="navbar-end pr-6 hidden lg:flex">
+                <ul class="menu menu-horizontal p-0 text-white font-sans antialiased uppercase font-medium">
+                    {liItems}
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
