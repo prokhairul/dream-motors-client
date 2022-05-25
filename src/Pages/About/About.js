643ccac5d60c37617bc5dd98605dc6b38d090244
vakhairulislam <tubebusy@gmail.com>
2022-05-25 17:56:39 +0600

    About us page Content Added

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <h2>This is About Us Page</h2>
+        <div className='container text-center w-8/12 mt-10 mb-28'>
+            <h2 className='text-3xl font-bold text-slate-900 uppercase mb-7'>About The Company</h2>
+            <p className='text-2xl font-mono'>Riding has always been about breaking free. As a band of renegades out to ride the road on our own terms, we take that idea seriously. That means a healthy respect for tradition but also the need to forge our own path. It means crafting components so every rider can escape conformity. And most of all, it means precision engineering that elevates both the form and function of the riding experience as we deliver world-class performance motorcycle parts and accessories. Dream Motors has been a pretty quiet, heads down working company. We've stayed quietly to the periphery of the Harley-Davidson™ scene. We quietly keep our foot in with the designers, innovators and performance minded folks in the Harley-Davidson™ community, for the most part in the West.</p>
+
         </div>
     );
 };
